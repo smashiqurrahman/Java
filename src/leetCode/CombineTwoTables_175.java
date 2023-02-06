958cadd4207de19305df7843ceacdf2ca552ecaf
smashiqurrahman <ashiqurrahmansm@gmail.com>
2023-02-07 03:05:46 +0600

    leetcode 2 & 176

--- src/leetCode/CombineTwoTables_175.java
+++ src/leetCode/CombineTwoTables_175.java
@@ -4,8 +4,9 @@ public class CombineTwoTables_175 {
 
 	/* Database Problem - MySql */
 
-//	Write your MySQL query statement below
-//	SELECT firstName, lastName , city, state
-//	FROM Person
-//	LefT JOIN Address ON Person.personId = Address.personId;
+	
+//	 SELECT firstName, lastName , city, state 
+//	 FROM Person 
+//	 LefT JOIN Address ON Person.personId = Address.personId;
+	
 }
---
+++ src/leetCode/SecondHighesSalary_176.java
@@ -0,0 +1,12 @@
+package leetCode;
+
+public class SecondHighesSalary_176 {
+	/* Database Problem - MySql */
+
+//	SELECT IFNULL(
+//		    (SELECT DISTINCT Salary
+//		    FROM Employee
+//		    ORDER BY Salary DESC
+//		    LIMIT 1,1
+//		), NULL) as SecondHighestSalary
+}
