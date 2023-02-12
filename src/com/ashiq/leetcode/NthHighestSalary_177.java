fc9296ea81d3086a4d7db168323c28b79d1f856b
smashiqurrahman <ashiqurrahmansm@gmail.com>
2023-02-13 01:45:59 +0600

    leetcode 177

---
+++ src/com/ashiq/leetcode/NthHighestSalary_177.java
@@ -0,0 +1,15 @@
+package com.ashiq.leetcode;
+
+public class NthHighestSalary_177 {
+//	CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+//	BEGIN
+//	    SET N = N-1;
+//	  RETURN (
+//	      # Write your MySQL query statement below.      
+//	          SELECT DISTINCT salary
+//	          FROM Employee
+//	          ORDER BY Salary DESC
+//	          LIMIT N, 1      
+//	  );
+//	END
+}
